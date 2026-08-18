f0ed16074da22b6fe7e53076d580dd69b755512e
hui.wang <hui.wang@contentsquare.com>
2026-08-18 14:31:51 +0200

    Revert "nvim: create missing dirs on save"
    This reverts commit 121ef4cc1e52fb2f5e28fcb04de882ea65af9e40.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -11,22 +11,4 @@ vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.breakindent = true
 
--- Auto-create missing parent directories on save
-local mkdir_augroup = vim.api.nvim_create_augroup("mkdir_on_save", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = mkdir_augroup,
-  callback = function(args)
-    local path = vim.api.nvim_buf_get_name(args.buf)
-
-    if path == "" or path:match("^%w+://") then
-      return
-    end
-
-    local dir = vim.fn.fnamemodify(path, ":p:h")
-    if vim.fn.isdirectory(dir) == 0 then
-      vim.fn.mkdir(dir, "p")
-    end
-  end,
-})
-
 
