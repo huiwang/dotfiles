bd6cfc77de6a6dac86d7817bef29a1b476db52e2
hui.wang <hui.wang@contentsquare.com>
2026-08-25 10:15:58 +0200

    Optimize Neovim for agent change reviews

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -3,8 +3,21 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Show line numbers
+-- Show line numbers and keep change markers visible
 vim.opt.number = true
+vim.opt.signcolumn = "yes"
+
+-- Agent processes often modify files behind Neovim's back.
+vim.opt.autoread = true
+vim.opt.updatetime = 300
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
+  desc = "Reload files changed outside Neovim",
+  command = "silent! checktime",
+})
+
+-- Keep review context and local recovery history.
+vim.opt.scrolloff = 6
+vim.opt.undofile = true
 
 -- Enable line wrapping at word boundaries
 vim.opt.wrap = true
--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -11,6 +11,9 @@ return {
     
     -- Keymap for Neogit
     vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "Neogit" })
+    vim.keymap.set("n", "<leader>gd", "<cmd>DiffviewOpen<cr>", { desc = "Review all working-tree changes" })
+    vim.keymap.set("n", "<leader>gD", "<cmd>DiffviewClose<cr>", { desc = "Close diff review" })
+    vim.keymap.set("n", "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", { desc = "Current file history" })
     vim.keymap.set("n", "<leader>gw", function()
       require('neogit').action('worktree', 'visit')()
     end, { desc = "Switch git worktree" })
